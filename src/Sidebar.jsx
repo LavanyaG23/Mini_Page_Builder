8aeabf183c1ad0c092038a122f440df9b0502cc7
LavanyaGokhale <92771328+LavanyaG23@users.noreply.github.com>
2024-04-29 19:22:07 +0530

    comments added for Sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,13 +1,16 @@
+// Importing necessary modules from React library
 import React, { useCallback } from "react";
-import PropTypes from "prop-types";
-import { nanoid } from "nanoid";
+import PropTypes from "prop-types"; // Importing PropTypes for type checking props
+import { nanoid } from "nanoid"; // Importing nanoid for generating unique IDs
 
+// Default text for different types of menu items
 const defaultText = {
   label: "This is a label.",
   input: "Input Field",
   button: "Button",
 };
 
+// Icon for indicating draggable item
 const DragIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -17,6 +20,7 @@ const DragIcon = () => (
     className="bi bi-six-dots"
     viewBox="0 0 16 16"
   >
+    {/* Six dots icon */}
     <rect x="3" y="3" width="4" height="4" rx="1" ry="1" fill="#d4d4d4" />
     <rect x="9" y="3" width="4" height="4" rx="1" ry="1" fill="#d4d4d4" />
     <rect x="3" y="8" width="4" height="4" rx="1" ry="1" fill="#d4d4d4" />
@@ -26,38 +30,49 @@ const DragIcon = () => (
   </svg>
 );
 
+// MenuItem component representing each draggable menu item
 const MenuItem = ({ type, setCurrentElementDetails, setShowModal }) => {
+  
+  // Callback function for handling drag end event
   const handleDragEnd = useCallback(
     (e) => {
+      // Set details of the current element
       setCurrentElementDetails({
-        id: nanoid(),
-        x: e.clientX,
-        y: e.clientY,
-        text: defaultText[type],
-        type,
-        fontSize: 14,
-        fontWeight: "normal",
+        id: nanoid(), // Generate unique id
+        x: e.clientX, // X coordinate of drop location
+        y: e.clientY, // Y coordinate of drop location
+        text: defaultText[type], // Default text based on type
+        type, // Type of menu item
+        fontSize: 14, // Default font size
+        fontWeight: "normal", // Default font weight
       });
+      // Show modal for customizing element details
       setShowModal(true);
     },
     [setCurrentElementDetails, type, setShowModal]
   );
 
+  // Render the draggable menu item
   return (
     <div
       className="draggable-element"
       draggable="true"
       onDragEnd={handleDragEnd}
     >
+      {/* Drag icon */}
       <DragIcon />
+      {/* Display type of menu item */}
       {type}
     </div>
   );
 };
 
+// Sidebar component displaying menu items
 const Sidebar = ({ setCurrentElementDetails, setShowModal }) => (
   <div className="sidebar">
+    {/* Sidebar header */}
     <div className="sidebar-header">BLOCKS</div>
+    {/* Map through menu item types and render MenuItem component */}
     {["label", "input", "button"].map((e) => (
       <MenuItem
         key={e}
@@ -69,15 +84,17 @@ const Sidebar = ({ setCurrentElementDetails, setShowModal }) => (
   </div>
 );
 
-export default Sidebar;
-
+// PropTypes for Sidebar component
 Sidebar.propTypes = {
-  setCurrentElementDetails: PropTypes.func.isRequired,
-  setShowModal: PropTypes.func.isRequired,
+  setCurrentElementDetails: PropTypes.func.isRequired, // Function to set current element details
+  setShowModal: PropTypes.func.isRequired, // Function to control modal visibility
 };
 
+// PropTypes for MenuItem component
 MenuItem.propTypes = {
-  type: PropTypes.string.isRequired,
-  setCurrentElementDetails: PropTypes.func.isRequired,
-  setShowModal: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired, // Type of menu item
+  setCurrentElementDetails: PropTypes.func.isRequired, // Function to set current element details
+  setShowModal: PropTypes.func.isRequired, // Function to control modal visibility
 };
+
+export default Sidebar;
