eb76cac9d658868acccfb9ea4253f2d726f74fa9
LavanyaGokhale <92771328+LavanyaG23@users.noreply.github.com>
2024-04-29 19:21:25 +0530

    comments added for main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Importing necessary modules from React library
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+// Importing the main component of the application
+import App from './App.jsx';
+
+// Using ReactDOM.createRoot() to create a root element in the HTML document
+// and setting it to render the main component of the application
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  // Wrapping the main component with <React.StrictMode> for additional checks and warnings
   <React.StrictMode>
-    <App />
+
+    <App /> {/* Rendering the main component */}
+    
   </React.StrictMode>,
-)
+
+);
